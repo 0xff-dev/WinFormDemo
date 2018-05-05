db5e48986394074c9be8735929682d0fe3c31c93
stevenshuang <stevenshuang521@gmail.com>
2018-05-05 19:26:40 +0800

    Start program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Windows.Forms;
+using SetUp.Main;
+using SetUp.MyForm;
+using SetUp.Common;
+
+class Program{
+	public static void Main(string[] args){
+		FormLogin fl = new FormLogin ();
+		fl.ShowDialog ();
+		if (fl.DialogResult == DialogResult.OK) {
+			Application.Run (new Mainwindow ());
+		}
+	}
+}
