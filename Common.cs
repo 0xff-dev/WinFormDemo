de046364c7007dde14275c464dc4fb0cd9fa8ca3
stevenshuang <stevenshuang521@gmail.com>
2018-05-05 19:25:11 +0800

    Global variables and Commod.dll

---
+++ Common.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace SetUp.Common{
+	public static class common{
+		private static bool isLogin = false;
+		private static string teacherId = null;
+        private static string courseName = null;
+		public static bool IsLogin{
+			get{
+				return isLogin;
+			}
+			set{
+				isLogin = value;
+			}
+		}
+		public static string TeacherId{
+			get {
+				return teacherId;
+			}
+			set {
+				teacherId=value;
+			}
+		}
+        public static string CourseName{
+            get{
+                return courseName;
+            }
+            set{
+                courseName = value;
+            }
+        }
+	}
+}
